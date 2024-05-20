0b8b72d2a8b0b64b66bd57410a965163177af662
Scarlet1107 <shougo.ushiro@gmail.com>
2024-05-20 12:11:33 +0900

    make header and building settlement button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import React from "react";
+import { useState } from "react";
 
 export default function Home() {
+
+
+  type Settlement = {
+    name: string;
+    id: number;
+    resources: [string, string, string];
+    numbers: [number, number, number];
+}
+
   return (
     <main className="">
+      <header className="text-3xl bg-blue-200 p-2 flex justify-center font-medium">カタン確率計算アプリ</header>
       <div className="">
-        Welcome to my page
+        <div>Welcome to my page. Explanation of this page is below.</div>
+        <button className="button">開拓地を追加</button>
       </div>
     </main>
   );
