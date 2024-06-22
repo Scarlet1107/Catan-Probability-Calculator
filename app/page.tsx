e02b9b81a5326d1793147d21db32593a78735cf3
scarlet7 <shougo.ushiro@gmail.com>
2024-06-22 18:16:31 +0900

    Implement loading page. It just wait 2 seconds

--- app/page.tsx
+++ app/page.tsx
@@ -28,32 +28,63 @@ ChartJS.register(
 );
 
 export default function App() {
-  return (
-    <SettlementsProvider>
-      <CatanApp />
-    </SettlementsProvider>
-  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    document.body.style.overflowY = "scroll"; // 常にスクロールバーを表示
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer); // クリーンアップ関数でタイマーをクリア
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="bg-custom-bg bg-cover bg-center flex items-center justify-center h-screen">
+        <div className="square-spin-1 mr-8"></div>
+        <div className="text-5xl">Now Loading<span className="dots fixed font-bold"></span></div>
+      </div>
+    );
+  } else {
+    return (
+      <SettlementsProvider>
+        <CatanApp />
+      </SettlementsProvider>
+    );
+  }
 }
 
 function CatanApp() {
   const { settlements, setSettlements } = useSettlements();
   const initialRender = useRef(true);
   const [isNumericMode, setIsNumericMode] = useState<boolean>(true);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
+    setIsVisible(true);
     const savedSettlements = localStorage.getItem("settlements");
     if (savedSettlements !== null && initialRender.current) {
       initialRender.current = false;
       setSettlements(JSON.parse(savedSettlements));
     }
-  }, []);
+  }, [setSettlements]);
 
   return (
-    <main className="bg-custom-bg bg-cover bg-center">
-      <Header isNumericMode = {isNumericMode} setIsNumericMode = {setIsNumericMode}/>
-      <div className="flex h-screen justify-center bg-white">
-        <ResourceInputPanel />
-        <CalculationResultPanel isNumericMode = {isNumericMode}/>
+    <main className="bg-custom-bg bg-cover bg-center h-screen">
+      <div
+        className={`opacity-0 ${
+          isVisible ? "opacity-100" : ""
+        } transition-opacity duration-1000`}
+      >
+        <Header
+          isNumericMode={isNumericMode}
+          setIsNumericMode={setIsNumericMode}
+        />
+        <div className="flex h-screen justify-center bg-white">
+          <ResourceInputPanel />
+          <CalculationResultPanel isNumericMode={isNumericMode} />
+        </div>
       </div>
     </main>
   );
